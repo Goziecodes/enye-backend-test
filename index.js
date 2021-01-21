c44318f4c9fe71d274242cce216481f8e322557c
goziecodes <ezechukwuchigozie@gmail.com>
2021-01-21 18:02:48 +0100

    succesfully corellected base currency and currencies to compare against and succesfully hit the exchange api and recieved data back

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 
 const port = 4000;
 
 app.get("/", (req, res) => {
+  res.send("welcome to home");
+});
+
+app.get("/api/rates/:base", (req, res) => {
+  const base = req.params.base;
+  const currency = Array.from(req.query.name.split(","));
+
+  fetch(
+    `https://api.exchangeratesapi.io/latest?base=${base}&symbols${currency}`
+  )
+    // .then((data) => JSON.parse(data))
+    .then((data) => data.json())
+    .then((data) => console.log(data));
+
   res.send("welcome to home");
 });
 
