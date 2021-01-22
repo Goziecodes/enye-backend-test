9df3a12289b6623cdd94901eb0784bf76da1f502
goziecodes <ezechukwuchigozie@gmail.com>
2021-01-22 15:46:18 +0100

    switched to axios for exchange request because it allowed me handle the errors in request better

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
+const { response } = require("express");
 const express = require("express");
 const fetch = require("node-fetch");
+const axios = require("axios");
 const app = express();
 
 const port = 4000;
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("welcome to home");
+  res.render("index");
 });
 
-app.get("/api/rates/:base", (req, res) => {
-  const base = req.params.base;
-  const currency = Array.from(req.query.name.split(","));
+app.get("/api/rates", (req, res) => {
+  const base = req.query.base;
+  const currency = req.query.currency.includes(",")
+    ? Array.from(req.query.currency.split(","))
+    : req.query.currency;
 
-  fetch(
-    `https://api.exchangeratesapi.io/latest?base=${base}&symbols${currency}`
-  )
-    // .then((data) => JSON.parse(data))
-    .then((data) => data.json())
-    .then((data) => console.log(data));
-
-  res.send("welcome to home");
+  axios
+    .get(
+      `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${currency}`
+    )
+    .then((resp) => {
+      const response = resp.data;
+      console.log(response);
+      const formatResult = {
+        results: {
+          base: response.base,
+          date: response.date,
+          rates: {
+            ...response.rates,
+          },
+        },
+      };
+      res.send(formatResult);
+      // return formatResult;
+    })
+    .catch((err) => {
+      // console.log(err.response.status, "oooooo");
+      // console.log(err.response.data);
+      console.log(err.response.status);
+      const errorResponse = {
+        status: err.response.status,
+        message: err.response.data.error,
+      };
+      res.send(errorResponse);
+    });
 });
 
 app.listen(port, () => {
